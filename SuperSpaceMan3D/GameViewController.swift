68b28c62a8d54e209401ab9ebb02e28b8f78297d
Romy Ilano <romy.ilano@gmail.com>
2018-04-17 20:55:21 -0700

    Initial Commit

---
+++ SuperSpaceMan3D/GameViewController.swift
@@ -0,0 +1,20 @@
+//
+//  GameViewController.swift
+//  SuperSpaceMan3D
+//
+//  Created by Romy Ilano on 4/17/18.
+//  Copyright © 2018 Romy Ilano. All rights reserved.
+//
+
+import UIKit
+import QuartzCore
+import SceneKit
+
+class GameViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view = SCNView()
+    }
+}
