4c4dbe21e72b2beb339c5e8ad05de8fff73d6a5c
Romy Ilano <romy.ilano@gmail.com>
2018-04-18 08:21:39 -0700

    making more obstacles

--- SuperSpaceMan3D/Obstacles.swift
+++ SuperSpaceMan3D/Obstacles.swift
@@ -22,4 +22,27 @@ class Obstacles: NSObject {
         
         return pyramidNode
     }
+    
+    class func GlobeNode() -> SCNNode {
+        let globe = SCNSphere(radius: 15.0)
+        let globeNode = SCNNode(geometry: globe)
+        globeNode.position = SCNVector3Make(20, 40, -50)
+        globeNode.geometry?.firstMaterial?.diffuse.contents = UIColor.red
+        return globeNode
+    }
+    
+    class func BoxNode() -> SCNNode {
+        let box = SCNBox(width: 10, height: 10, length: 10, chamferRadius: 0)
+        let boxNode = SCNNode(geometry: box)
+        boxNode.geometry?.firstMaterial?.diffuse.contents = UIColor.brown
+        boxNode.position = SCNVector3Make(0, 10, -20)
+        return boxNode
+    }
+    
+    class func CylinderNode() -> SCNNode {
+        let cynlinderNode = SCNNode(geometry: SCNCylinder(radius: 3, height: 12))
+        cynlinderNode.geometry?.firstMaterial?.diffuse.contents = UIColor.green
+        cynlinderNode.position = SCNVector3Make(14, 0, -25)
+        return cynlinderNode
+    }
 }
